a01fa8acc3413800e1d9aedc16db3b73ee591ba6
Dzamora89 <92052822+Dzamora89@users.noreply.github.com>
2023-01-31 21:13:32 +0100

    Sesiones implementadas. Comprobacion Generada.

---
+++ funciones.php
@@ -0,0 +1,33 @@
+<?php
+
+function generarClave(): array
+{
+    $clave = [];
+    while (count($clave) < 4) {
+        $numeroAleatorio = rand(0,9);
+        if (!in_array($numeroAleatorio,$clave)){
+            $clave[] = $numeroAleatorio;
+        }
+    }
+    return $clave;
+}
+
+function comprobarJugada($array,$clave)
+{
+    $resultado = array();
+    for ($i = 0; $i < count($clave); $i++) {
+           if ($array[$i] == $clave[$i]){
+               $resultado[$i] = 2;
+           }else{
+               for ($j = 0; $j < count($array); $j++) {
+                   if (($array[$i] == $clave[$j]) && ($i != $j)){
+                       $resultado[$i] = 1;
+                   }
+               }
+           }
+           if (!isset($resultado[$i]) ){
+               $resultado[$i] = 0;
+           }
+    }
+    $_SESSION['resultados'] = $resultado;
+}
---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+include_once "funciones.php";
+session_start();
+if (!empty($_SESSION['clave'])){
+    $clave = $_SESSION['clave'];
+}else {
+    $clave = generarClave();
+    $_SESSION['clave'] = $clave;
+}
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MisterMind</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<h1>La Clave es <?php foreach ($clave as $item) {
+        echo $item . " ";
+} ?></h1>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <input  type="Number" name="digito1">
+    <input type="Number" name="digito2">
+    <input type="Number" name="digito3">
+    <input type="Number" name="digito4">
+    <button name="enviado" value="si" type="submit">Enviar</button>
+</form>
+<?php
+    if (!empty($_POST['enviado'])){
+        $jugada = array($_POST['digito1'],$_POST['digito2'],$_POST['digito3'],$_POST['digito4']);
+        $_SESSION['jugada'] = $jugada;
+    }
+comprobarJugada($_SESSION['jugada'],$_SESSION['clave']);
+
+
+    $resultados = $_SESSION['resultados'];
+foreach ($resultados as $resultado) {
+    if ($resultado == 2){
+        echo "Muerto <br>";
+    }
+    if ($resultado == 1){
+        echo "Tocado <br>";
+    }
+    if ($resultado == 0){
+        echo "Fallo <br>";
+    }
+}
+
+
+?>
+</body>
+</html>
